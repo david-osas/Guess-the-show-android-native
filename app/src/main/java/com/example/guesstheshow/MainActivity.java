1455a9d5daa72825e6e048d56079b1a965c075de
david-osas <azamegbeosarumense@gmail.com>
2020-09-28 00:11:56 +0100

    Added functionality to login and logout

--- app/src/main/java/com/example/guesstheshow/MainActivity.java
+++ app/src/main/java/com/example/guesstheshow/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.guesstheshow;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,10 +9,14 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.guesstheshow.databinding.ActivityMainBinding;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,21 +26,44 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+
+        invalidateOptionsMenu();
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
         getMenuInflater().inflate(R.menu.user,menu);
 
-        MenuItem highscores = menu.findItem(R.id.highscores);
 
-        highscores.setVisible(false);
 
         return true;
     }
 
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        auth = FirebaseAuth.getInstance();
+        super.onPrepareOptionsMenu(menu);
+        MenuItem highscores = menu.findItem(R.id.highscores);
+        MenuItem user = menu.findItem(R.id.login);
+
+        if(auth.getCurrentUser() == null){
+            highscores.setVisible(false);
+            user.setTitle(getString(R.string.login));
+        }else{
+            highscores.setVisible(true);
+            user.setTitle(getString(R.string.logout));
+        }
+
+        return true;
+    }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         super.onOptionsItemSelected(item);
+        auth = FirebaseAuth.getInstance();
 
         if(item.getItemId() == R.id.highscores){
             Intent intent = new Intent(this, HighscoresActivity.class);
@@ -45,12 +73,16 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(this, LoginActivity.class);
             startActivity(intent);
 
-        }else if(item.getTitle().toString().equals("Logout")){
-
+        }else if(item.getTitle().toString().equalsIgnoreCase("Logout")){
+            auth.signOut();
+            Toast.makeText(MainActivity.this,getString(R.string.logoutMessage),Toast.LENGTH_SHORT).show();
+            invalidateOptionsMenu();
         }
 
         return true;
     }
+
+
 
     public void showChoices(View view){
         String tag = view.getTag().toString();
