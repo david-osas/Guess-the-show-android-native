2311d55b89d63b871573bbec2fd1f19ebc27296b
david-osas <azamegbeosarumense@gmail.com>
2020-10-01 00:07:26 +0100

    Removed login and leader board

--- app/src/main/java/com/example/guesstheshow/MainActivity.java
+++ app/src/main/java/com/example/guesstheshow/MainActivity.java
@@ -1,85 +1,18 @@
 package com.example.guesstheshow;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
-
-import com.example.guesstheshow.databinding.ActivityMainBinding;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        invalidateOptionsMenu();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate(R.menu.user,menu);
-
-
-
-        return true;
-    }
-
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        auth = FirebaseAuth.getInstance();
-        super.onPrepareOptionsMenu(menu);
-        MenuItem highscores = menu.findItem(R.id.highscores);
-        MenuItem user = menu.findItem(R.id.login);
-
-        if(auth.getCurrentUser() == null){
-            highscores.setVisible(false);
-            user.setTitle(getString(R.string.login));
-        }else{
-            highscores.setVisible(true);
-            user.setTitle(getString(R.string.logout));
-        }
-
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        super.onOptionsItemSelected(item);
-        auth = FirebaseAuth.getInstance();
-
-        if(item.getItemId() == R.id.highscores){
-            Intent intent = new Intent(this, HighscoresActivity.class);
-            startActivity(intent);
-
-        }else if(item.getItemId() == R.id.login && !item.getTitle().toString().equals("Logout")){
-            Intent intent = new Intent(this, LoginActivity.class);
-            startActivity(intent);
-
-        }else if(item.getTitle().toString().equalsIgnoreCase("Logout")){
-            auth.signOut();
-            Toast.makeText(MainActivity.this,getString(R.string.logoutMessage),Toast.LENGTH_SHORT).show();
-            invalidateOptionsMenu();
-        }
-
-        return true;
     }
 
 
--- app/src/main/java/com/example/guesstheshow/ResultsActivity.java
+++ app/src/main/java/com/example/guesstheshow/ResultsActivity.java
@@ -1,5 +1,6 @@
 package com.example.guesstheshow;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,10 +9,6 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.example.guesstheshow.databinding.ActivityResultsBinding;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
 
@@ -29,7 +26,6 @@ public class ResultsActivity extends AppCompatActivity {
         Intent intent = getIntent();
         category = intent.getStringExtra("category");
         setDetails(intent.getIntArrayExtra("scores"));
-        addToCloudDB(intent.getIntArrayExtra("scores")[2],category);
 
     }
 
@@ -41,29 +37,6 @@ public class ResultsActivity extends AppCompatActivity {
         intent.putExtra("category",category);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
-    }
-
-    public void addToCloudDB(int score, String category){
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        FirebaseAuth auth = FirebaseAuth.getInstance();
-        if(auth.getCurrentUser() != null){
-            String email = auth.getCurrentUser().getEmail();
-            String[] emailSplit = email.split("@");
-
-            HashMap<String,Object> data = new HashMap<>();
-            data.put("title",emailSplit[0]);
-            data.put("category",category);
-            data.put("score",score);
-
-            db.collection("leaderBoard")
-                    .add(data)
-                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                        @Override
-                        public void onSuccess(DocumentReference documentReference) {
-                            Toast.makeText(ResultsActivity.this,getString(R.string.cloudDBSuccess),Toast.LENGTH_SHORT).show();
-                        }
-                    });
-        }
     }
 
     public void setDetails(int[] scores){
